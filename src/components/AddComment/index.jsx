142a24b378ab94f0644d9cd13f003b01e8201bc9
Anita404 <writingonbookends@gmail.com>
2024-03-19 17:21:15 +0100

    Change Responsive Layout and Add Flex/Remove Margins

--- src/components/AddComment/index.jsx
+++ src/components/AddComment/index.jsx
@@ -1,19 +1,49 @@
+import { useState } from "react";
+import { MOBILE_WIDTH } from "../../utils/constants";
 import {
   CommentContainer,
   InputContainer,
   InputText,
+  MobileContainer,
   ProfilePicture,
   SendButton,
 } from "./styles";
+import { useEffect } from "react";
 
 const AddComment = () => {
+  const [screenWidth, setScreenWidth] = useState(innerWidth);
+
+  useEffect(() => {
+    const updateScreenWidth = setScreenWidth(innerWidth);
+
+    const resize = addEventListener("resize", updateScreenWidth);
+
+    return () => {
+      removeEventListener(resize, updateScreenWidth);
+    };
+  }, []);
+
   return (
     <CommentContainer>
-      <ProfilePicture />
-      <InputContainer>
-        <InputText placeholder="Add a comment..." />
-      </InputContainer>
-      <SendButton> SEND </SendButton>
+      {screenWidth <= MOBILE_WIDTH ? (
+        <>
+          <InputContainer>
+            <InputText placeholder="Add a comment..." />
+          </InputContainer>
+          <MobileContainer>
+            <ProfilePicture />
+            <SendButton> SEND </SendButton>
+          </MobileContainer>
+        </>
+      ) : (
+        <>
+          <ProfilePicture />
+          <InputContainer>
+            <InputText placeholder="Add a comment..." />
+          </InputContainer>
+          <SendButton> SEND </SendButton>
+        </>
+      )}
     </CommentContainer>
   );
 };

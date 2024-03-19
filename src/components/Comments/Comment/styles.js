7992014581cd7ae493879b0304cbef3077b1172a
Anita404 <writingonbookends@gmail.com>
2024-03-19 17:21:15 +0100

    Debugging Rendering Issue

---
+++ src/components/Comments/Comment/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  /* background: white; */
+  width: 730px;
+  height: 167px;
+  flex-shrink: 0;
+  border-radius: 8px;
+`;
